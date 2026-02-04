1c2e639d8750057fc41cf83cfead464b1b3cea4f
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2026-02-04 15:55:12 +0630

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("Hi ");
+
+bool isSuccess = int.TryParse(Console.ReadLine(),out int num);
+
+if (isSuccess)
+{
+    Console.WriteLine(num); 
+}
