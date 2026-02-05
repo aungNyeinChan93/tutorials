e07d2c56513ce1ede292d284bd52dcdefd9a3f58
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2026-02-05 18:42:34 +0630

    update: add crud blogs

--- ConsoleApp3/AdoDotNet/AdoBlog.cs
+++ ConsoleApp3/AdoDotNet/AdoBlog.cs
@@ -83,5 +83,111 @@ namespace ConsoleApp3.AdoDotNet
             connection.Close();
 
         }
+
+        public void ReadById(SqlConnection connection)
+        {
+            connection.Open();
+
+            Console.WriteLine("Enter Blog Id");
+            
+            bool isSuccess = int.TryParse(Console.ReadLine(), out int BlogId);
+            if(!isSuccess)
+            {
+                Console.WriteLine("Blog id must be interger ");
+                return;
+            }
+
+            string query = @"SELECT [BlogId]
+                  ,[Title]
+                  ,[Description]
+                  ,[AuthorName]
+                  ,[DeleteFlag]
+              FROM [KSLH_01].[dbo].[Tbl_Blogs]
+              where Tbl_Blogs.BlogId = @BlogId";
+
+
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@BlogId", BlogId);
+            DataTable tb = new DataTable();
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            adapter.Fill(tb);
+
+            if(tb.Rows.Count < 1)
+            {
+                Console.WriteLine($"Blog Data not found!");
+                return;
+            }
+
+            DataRow blog = tb.Rows[0];
+
+            Console.WriteLine($"Blog title is {blog["title"]} and author name is {blog["AuthorName"]}");
+
+            connection.Close();
+        }
+        public void Update(SqlConnection connection)
+        {
+            connection.Open();
+
+            Console.WriteLine("Blog data update case ... ");
+
+            string query = @"
+            update [KSLH_01].[dbo].[Tbl_Blogs] 
+            set	
+	            Title = @Title,
+	            Description = @Description,
+	            AuthorName = @Author,
+	            DeleteFlag = 0
+            where [KSLH_01].[dbo].[Tbl_Blogs].BlogId = @BlogId";
+
+            Console.Write("Enter Update Title ");
+            string title = Console.ReadLine();
+
+
+            Console.Write("Enter Update Description ");
+            string desc = Console.ReadLine();
+
+            Console.Write("Enter Update AuthorName ");
+            string author = Console.ReadLine();
+
+            Console.Write("Enter Update Blog Id ");
+            string blogId = Console.ReadLine();
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue ("@title", title);
+            cmd.Parameters.AddWithValue ("@Description", desc);
+            cmd.Parameters.AddWithValue ("@Author", author);
+            cmd.Parameters.AddWithValue ("@BlogId",blogId );
+
+            int affectRows = cmd.ExecuteNonQuery();
+
+            Console.WriteLine(affectRows >=1 ? "Update success":"update fail");
+
+            connection.Close();
+
+        }
+
+        public void Delete(SqlConnection connection)
+        {
+            connection.Open();
+
+            Console.WriteLine("Blog Delete ....");
+
+            Console.WriteLine("Enter Blog Id");
+            string blogId = Console.ReadLine();
+
+            string query = @"delete from [KSLH_01].[dbo].[Tbl_Blogs]
+                where [KSLH_01].[dbo].[Tbl_Blogs].BlogId = @BlogId
+                ";
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("BlogId",blogId);
+
+            int affectRow = cmd.ExecuteNonQuery();
+
+            Console.WriteLine(affectRow >=1 ? "Delete success":"delete fail");
+
+            connection.Close();
+        }
     }
 }
--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -3,15 +3,31 @@
 
 
 using ConsoleApp3.AdoDotNet;
+using System.ComponentModel.DataAnnotations;
 using System.Data;
 using System.Data.SqlClient;
 using System.Runtime.InteropServices.ComTypes;
 
+
 SqlConnection connection = new SqlConnection($"Data Source=.;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Application Name=\"SQL Server Management Studio\"");
 
-
 AdoBlog readBlog = new AdoBlog();
+
+//Read data
 readBlog.Read(connection);
+
+//// Create Data
 readBlog.Create(connection);
+
+//Edit Data
+readBlog.ReadById(connection);
+
+//Update Data
+readBlog.Update(connection);
+
+// Delete Data
+readBlog.Delete(connection);
+
+
 
 
